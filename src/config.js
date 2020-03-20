f67357180218bb89d5a661c7a4b2a2118eb043de
Tricia Sykes <tlsykes98@gmail.com>
2020-03-19 21:16:23 -0700

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+        REGION: 'us-west-2',
+        BUCKET: "aws-sam-cli-managed-default-samclisourcebucket-kg32v7mkppi9"
+    },
+
+    apiGateway: {
+        REGION: "us-west-2",
+        URL: "https://p3ojwstxnfc6ngc3vqbczwmnfm.appsync-api.us-west-2.amazonaws.com/graphql"
+    },
+    cognito: {
+        REGION: "us-west-2",
+        USER_POOL_ID: "us-west-2_hUkPGtQGz",
+        APP_CLIENT_ID: "6bds64b80seihk6ih44mqtsvqe"
+    }
+
+}
